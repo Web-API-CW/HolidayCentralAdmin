e3dfd72f72b83aa680f17e84308111e1898acc26
Lakna Attigala <Lakna0804@gmail.com>
2023-05-04 22:11:02 +0530

    -> Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Login from './components/login.component'
+import SignUp from './components/signup.component'
+import CsvUploadFlight from './components/flightdata';
+import CsvUploadHotel from './components/hoteldata';
+import CsvUploadPackage from './components/packagedata';
+import UserTable from './components/usertable';
+import FlightTable from './components/flighttable';
+import HotelTable from './components/hoteltable';
+import PackageTable from './components/packagetable';
+import Dashboard from './components/dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={'/dashboard'}>
+              HolidayCentral
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-in'}>
+                    Login
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/sign-up'}>
+                    New User
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/ftable'}>
+                    User Details
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/Flight-Csv'}>
+                    Flight CSV Upload
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/flighttable'}>
+                    Flight Details
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/Hotel-Csv'}>
+                    Hotel CSV Upload
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/hoteltable'}>
+                    Hotel Details
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/Package-Csv'}>
+                    Package CSV Upload
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/packagetable'}>
+                    Package Details
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+              <Route path="/Flight-Csv" element={<CsvUploadFlight/>} />
+              <Route path="/Hotel-Csv" element={<CsvUploadHotel/>} />
+              <Route path="/Package-Csv" element={<CsvUploadPackage/>} />
+              <Route path="/ftable" element={<UserTable/>} />
+              <Route path="/flighttable" element={<FlightTable/>} />
+              <Route path="/hoteltable" element={<HotelTable/>} />
+              <Route path="/packagetable" element={<PackageTable/>} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  )
 }
-
-export default App;
+export default App
